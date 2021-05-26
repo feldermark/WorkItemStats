c5bee79e494764056cd02d0cff1129df081009ca
Mark Felder <felder.mark@gmail.com>
2021-05-26 11:38:44 -0500

    adding error handling and trying to fix an issue where I create double 'new branch' menu items

--- src/work-item-stats.ts
+++ src/work-item-stats.ts
@@ -29,39 +29,28 @@ const workitemstats = {
   getMenuItems: (context: any) => {
     const wids = context.workItemIds;
 
-    // Uses the plural of cards if more than one are selected
-    // let menuItemText = "One Selected";
-    // if (wids && wids.length > 1) {
-    //   menuItemText = "Multiple Selected";
-    // }
-
-    // console.log(wids.length + "selected");
-    // console.log(context);
-    // console.log(wids);
-
-    // getWorkItems(wids).then(workItems => workItems.reduce((sum, current) => sum + current.fields["System.StoryPoints"], 0));
-    // menuItemText = sum;
-
     return getWorkItems(wids).then(workItems => {
-      let workItemSum: number = 0;
-      workItemSum = workItems.filter(workItem => workItem.fields["Microsoft.VSTS.Scheduling.StoryPoints"] !== undefined).reduce((sum, current) => sum + current.fields["Microsoft.VSTS.Scheduling.StoryPoints"], 0);
+      try {
+        let workItemSum: number = 0;
+        workItemSum = workItems.filter(workItem => workItem.fields["Microsoft.VSTS.Scheduling.StoryPoints"] !== undefined).reduce((sum, current) => sum + current.fields["Microsoft.VSTS.Scheduling.StoryPoints"], 0);
 
-      let menuItemText = workItemSum + " story points";
-      if (workItemSum === 1) {
-        menuItemText = workItemSum + " story point";
+        let menuItemText = workItemSum + " story points";
+        if (workItemSum === 1) {
+          menuItemText = workItemSum + " story point";
+        }
+        return [
+          {
+            // action: // This has no action right now
+            icon: "img/icon.png",
+            text: menuItemText,
+            disabled: true
+          } as IContributedMenuItem];
       }
-
-      // Add if here to not return a menu item if we have no points?
-
-      return [
-        {
-          // action: // This has no action right now
-          icon: "img/icon.png",
-          text: menuItemText,
-          // title: menuItemText,
-          disabled: true // Disabling this button until I decide to make it do something. It could show an popup with additional stats
-        } as IContributedMenuItem];
-    });
+      catch (e) {
+        console.log(e);
+        return [] as IContributedMenuItem[];
+      }
+      });
   }
 };
 
